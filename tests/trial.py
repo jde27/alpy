dcbb427893aa86a861e4babca872db8723433faf
Jonny Evans <jonny.holomorphic@gmail.com>
2017-02-25 12:05:45 +0000

    More detail in documentation and included an example session.

--- tests/trial.py
+++ tests/trial.py
@@ -1,33 +1,18 @@
 #!/usr/bin/python
 
 import finite_fields as ff
+import rationals as QQ
 import a_infinity as ainf
 
 K=ff.FF(2)
 A=ainf.BP(2,3,K,2)
-A.verify()
 M=A.yoneda(1)
 N=A.yoneda(2)
-P=M.twist(2)
-#print('DISPLAYING P')
-#P.display()
-print('*********************************')
-Q=P.twist(2)
-#print('DISPLAYING Q')
-#Q.display()
-print('*********************************')
-R=Q.twist(2)
-#print('DISPLAYING R')
-#R.display()
-print('*********************************')
-S=R.twist(2).twist(2)
-T=S.simplify()
-print(S.cpx(1).cohomology())
-print(T.cpx(1).cohomology())
-T.display()
-#S.cpx(1).verify()
-#R.cpx(2).verify()
-#print(P.cpx(1).cohomology())
-#print(Q.cpx(1).cohomology())
-#print(R.cpx(1).cohomology())
-
+# Enter braid word here:
+w=[1,2,1,2,1,2]
+P=M
+Q=N
+for n in w:
+    R,S=P.twist(n),Q.twist(n)
+    P,Q=R,S
+print(P.width(),Q.width())
